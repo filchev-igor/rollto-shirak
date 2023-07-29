34a9393c50d0bf6c8d834d101495822831b2fdd8
Filchev Igor <93984044+igor-filchev-zenitech@users.noreply.github.com>
2023-07-29 22:47:50 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // @ts-ignore
+  plugins: [react(), basicSsl()],
+  server: {
+    port: 3008,
+    strictPort: true,
+    https: true,
+    open: true,
+    headers: {
+      "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+      "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+      "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+    },
+  },
+  preview: {
+    port: 3045,
+    strictPort: true,
+    https: true,
+    open: true,
+    headers: {
+      "Strict-Transport-Security": "max-age=86400; includeSubDomains", // Adds HSTS options to your website, with a expiry time of 1 day
+      "X-Content-Type-Options": "nosniff", // Protects from improper scripts runnings
+      "X-Frame-Options": "DENY", // Stops your site being used as an iframe
+      "X-XSS-Protection": "1; mode=block", // Gives XSS protection to legacy browsers
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  worker: {
+    plugins: []
+  }
 })
